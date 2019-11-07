29aed04e367ee20eb5b467f6e8f7f460f022e3b8
Nathan Bhamra <P2406502@my365.dmu.ac.uk>
2019-11-07 18:23:48 +0000

    Commented the main

--- ShapesLibrary/ShapesLibrary/main.cpp
+++ ShapesLibrary/ShapesLibrary/main.cpp
@@ -5,7 +5,7 @@
 * with parameter inputs. In here the window for the SFML is opened.
 */
 
-
+// Including SFML Graphics Library, Input Output Library and all header file for every shape
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include "line.h"
@@ -17,25 +17,24 @@
 
 
 int main() {
-	sf::RenderWindow window(sf::VideoMode(1400, 210), "Shapes Library");
-	line lineShape(sf::Vector2f(190, 10), sf::Vector2f(10, 190));
+	sf::RenderWindow window(sf::VideoMode(1400, 210), "Shapes Library"); //Creating the window for the visuals. The size of the window and the name of the window is also stated
 
-	triangle triangleShape(sf::Vector2f(300, 10), sf::Vector2f(210, 190), sf::Vector2f(390, 190), sf::Vector2f(300, 10));
+	line lineShape(sf::Vector2f(190, 10), sf::Vector2f(10, 190)); //This will define the vectors for the 2 points to form the shape
 
-	square squareShape(sf::Vector2f(410, 10), sf::Vector2f(590, 10), sf::Vector2f(590, 190), sf::Vector2f(410, 190), sf::Vector2f(410, 10));
+	triangle triangleShape(sf::Vector2f(300, 10), sf::Vector2f(210, 190), sf::Vector2f(390, 190), sf::Vector2f(300, 10)); //This will define the vectors for the 4 points to form the shape
 
-	rectangle rectangleShape(sf::Vector2f(610, 10), sf::Vector2f(990, 10), sf::Vector2f(990, 190), sf::Vector2f(610, 190), sf::Vector2f(610, 10));
+	square squareShape(sf::Vector2f(410, 10), sf::Vector2f(590, 10), sf::Vector2f(590, 190), sf::Vector2f(410, 190), sf::Vector2f(410, 10)); //This will define the vectors for the 5 points to form the shape
 
-	pentagon pentagonShape(sf::Vector2f(1100, 10), sf::Vector2f(1010, 75), sf::Vector2f(1050, 190), sf::Vector2f(1150, 190), sf::Vector2f(1190, 75), sf::Vector2f(1100,10));
+	rectangle rectangleShape(sf::Vector2f(610, 10), sf::Vector2f(990, 10), sf::Vector2f(990, 190), sf::Vector2f(610, 190), sf::Vector2f(610, 10)); //This will define the vectors for the 5 points to form the shape
 
-	dot dotShape(sf::Vector2f(1300, 100), sf::Vector2f(1299, 101), sf::Vector2f(1301,101), sf::Vector2f(1300,100));
+	pentagon pentagonShape(sf::Vector2f(1100, 10), sf::Vector2f(1010, 75), sf::Vector2f(1050, 190), sf::Vector2f(1150, 190), sf::Vector2f(1190, 75), sf::Vector2f(1100,10)); //This will define the vectors for the 6 points to form the shape
 
-	while (window.isOpen()) {
-		//handle events
+	dot dotShape(sf::Vector2f(1300, 100), sf::Vector2f(1299, 101), sf::Vector2f(1301,101), sf::Vector2f(1300,100)); //This will define the vectors for the 4 points to form the shape
 
-		window.display();
-		//draw objects to window
-
+	while (window.isOpen()) { //All the events below will run once the window is open
+		window.display(); //Displays the window
+		
+		// Draws each shape to the screen using the data above
 		window.draw(triangleShape);
 		window.draw(lineShape);
 		window.draw(squareShape);
