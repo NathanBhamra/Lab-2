4110efa26eff6fe47d47e4449c254d6d4f92c99d
Nathan Bhamra <P2406502@my365.dmu.ac.uk>
2019-11-07 18:13:25 +0000

    Fully commented Line cpp and header

--- ShapesLibrary/ShapesLibrary/line.cpp
+++ ShapesLibrary/ShapesLibrary/line.cpp
@@ -1,23 +1,27 @@
 /*!
 \file line.cpp
+*
+* This file will store the functions to draw the shape with help of the shapes function which was included in the header file
+*
 */
 
-#include "line.h"
-#include <iostream>
+#include "line.h" // Including the header file
+#include <iostream> // Including input output functions
 
-using namespace std;
+using namespace std; // Defining what syntax we will use, this will save time instead of using std::
 
-line::line(sf::Vector2f a, sf::Vector2f b) : Shape(2)
+line::line(sf::Vector2f a, sf::Vector2f b) : Shape(2) //Using the function which was defined in the header with a constructor to define what a and b classes as. This will also run Shape twice as it has 2 points
 {
-	point1 = a;
+	// Redefining a and b to point1 and point2
+	point1 = a; 
 	point2 = b;
 
-	drawLine();
+	drawLine(); //This is going to run the drawLine function
 }
 
-void line::drawLine() {
-	points[0].position = point1;
-	points[0].color = sf::Color::White;
+void line::drawLine() { //Running the drawLine function
+	points[0].position = point1; // Stating the position of the point using an array called points
+	points[0].color = sf::Color::White; // Stating the colour of the point using an array called points
 
 	points[1].position = point2;
 	points[1].color = sf::Color::White;
--- ShapesLibrary/ShapesLibrary/line.h
+++ ShapesLibrary/ShapesLibrary/line.h
@@ -1,20 +1,22 @@
 /*!
 \file line.h
+*
+* This is the first shape that I will form. In this header file I am declaring the class and constructing the points using a Vector2f
+* data type. Having this in a class allows me to create multiple instances of it and have multiple 'lines' with different vectors.
 */
 
 #pragma once
-#include "SFML/Graphics.hpp"
-#include "Shape.h"
+#include "SFML/Graphics.hpp" //!< Importing the SFML Library
+#include "Shape.h" //!< Importing the Shapes class
 
-class line : public Shape {
-public:
+class line : public Shape { //!< This is declaring a class called line and inheriting the class Shape 
+public: //!< Public assess specifier, this means that everything below can be accessed by the main
 
-	//Line Constructor
-	line(sf::Vector2f a, sf::Vector2f b);
+	line(sf::Vector2f a, sf::Vector2f b); //!< This creates is a constructor for the variable line
 
-	//Variables for position
+	//!< This is creating 2 variables
 	sf::Vector2f point1;
 	sf::Vector2f point2;
 
-	void drawLine();
+	void drawLine(); //!< Running the function drawLine(), this is in the cpp
 };
