643fd7d31768acc6b511038587197c7c94cb5edc
Nathan Bhamra <P2406502@my365.dmu.ac.uk>
2019-11-08 14:31:19 +0000

    Added commets to lots of other code however had to stop as I'm running out of time

--- ShapesLibrary/ShapesLibrary/rectangle.cpp
+++ ShapesLibrary/ShapesLibrary/rectangle.cpp
@@ -1,28 +1,29 @@
 /*!
 \file rectangle.cpp
 *
-* Comments are the same for each shape, check the line shape for the comments.
-* The only difference is there are more points to form the shape.
+* This file will store the functions to draw the shape with help of the shapes function which was included in the header file
+*
 */
 
-#include "rectangle.h"
-#include <iostream>
+#include "rectangle.h" // Including the header file
+#include <iostream> // Including input output functions
 
-using namespace std;
+using namespace std; // Defining what syntax we will use, this will save time instead of using std::
 
-rectangle::rectangle(sf::Vector2f a, sf::Vector2f b, sf::Vector2f c, sf::Vector2f d, sf::Vector2f e) : Shape(5) {
+rectangle::rectangle(sf::Vector2f a, sf::Vector2f b, sf::Vector2f c, sf::Vector2f d, sf::Vector2f e) : Shape(5) { //Using the function which was defined in the header with a constructor to define what a and b... classes are.
+	// Redefining a,b... to point1,2...
 	point1 = a;
 	point2 = b;
 	point3 = c;
 	point4 = d;
 	point5 = e;
 
-	drawRectangle();
+	drawRectangle(); //This is going to run the draw function
 }
 
-void rectangle::drawRectangle() {
-	points[0].position = point1;
-	points[0].color = sf::Color::Magenta;
+void rectangle::drawRectangle() { //Running the draw function
+	points[0].position = point1;  // Stating the position of the point using an array called points
+	points[0].color = sf::Color::Magenta; // Stating the colour of the point using an array called points
 
 	points[1].position = point2;
 	points[1].color = sf::Color::Magenta;
--- ShapesLibrary/ShapesLibrary/triangle.cpp
+++ ShapesLibrary/ShapesLibrary/triangle.cpp
@@ -1,28 +1,29 @@
 /*!
 \file triangle.cpp
 *
-* Comments are the same for each shape, check the line shape for the comments.
-* The only difference is there are more points to form the shape.
+* This file will store the functions to draw the shape with help of the shapes function which was included in the header file
+*
 */
 
-#include "triangle.h"
-#include <iostream>
+#include "triangle.h" // Including the header file
+#include <iostream> // Including input output functions
 
-using namespace std;
+using namespace std; // Defining what syntax we will use, this will save time instead of using std::
 
-triangle::triangle(sf::Vector2f a, sf::Vector2f b, sf::Vector2f c, sf::Vector2f d) : Shape(4)
+triangle::triangle(sf::Vector2f a, sf::Vector2f b, sf::Vector2f c, sf::Vector2f d) : Shape(4) //Using the function which was defined in the header with a constructor to define what a and b... classes has.
 {
+	// Redefining a,b... to point1,2...
 	point1 = a;
 	point2 = b;
 	point3 = c;
 	point4 = d;
 
-	drawtriangle();
+	drawtriangle(); //This is going to run the draw function
 }
 
-void triangle::drawtriangle() {
-	points[0].position = point1;
-	points[0].color = sf::Color::Blue;
+void triangle::drawtriangle() { //Running the draw function
+	points[0].position = point1; // Stating the position of the point using an array called points
+	points[0].color = sf::Color::Blue; // Stating the colour of the point using an array called points
 
 	points[1].position = point2;
 	points[1].color = sf::Color::Blue;
